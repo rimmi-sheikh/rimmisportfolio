b450957e10873340782c43af511f64715f64fac5
Rimmal Sheikh <rimmalsheikh27@gmail.com>
2025-04-02 14:17:27 +0500

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -24,18 +24,7 @@ export const Footer = () => {
         vertical="center"
       >
         <Text variant="body-default-s" onBackground="neutral-strong">
-          <Text onBackground="neutral-weak">© {currentYear} /</Text>
-          <Text paddingX="4">{person.name}</Text>
-          <Text onBackground="neutral-weak">
-            {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
-            / Build your portfolio with{" "}
-            <SmartLink
-              style={{ marginLeft: "-0.125rem" }}
-              href="https://once-ui.com/templates/magic-portfolio"
-            >
-              Once UI
-            </SmartLink>
-          </Text>
+          © {currentYear} Rimmi Sheikh. Do not redistribute—this isn't Minecraft loot! ⚔️
         </Text>
         <Flex gap="16">
           {social.map(
