4ffd997814639d034aa4da2199753ed799190835
Rimmal Sheikh <rimmalsheikh27@gmail.com>
2025-04-05 15:43:34 +0500

    hey

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -63,8 +63,8 @@ const home = {
 };
 
 const about = {
-  label: "rimmi ~ about",
-  title: "ABout",
+  label: "About",
+  title: "About",
   description: `Meet ${person.name}, ${person.role} from ${person.location}`,
   tableOfContent: {
     display: true,
@@ -153,7 +153,7 @@ const about = {
 };
 
 const blog = {
-  label: "rimmi ~ blog",
+  label: "Blog",
   title: "Blog",
   description: `Read what ${person.name} has been up to recently`,
   // Create new blog posts by adding a new .mdx file to app/blog/posts
@@ -162,7 +162,7 @@ const blog = {
 
 const work = {
   label: "Work",
-  title: "rimmi ~ work",
+  title: "Work",
   description: `Design and dev projects by ${person.name}`,
   // Create new project pages by adding a new .mdx file to app/blog/posts
   // All projects will be listed on the /home and /work routes
@@ -170,7 +170,7 @@ const work = {
 
 const gallery = {
   label: "Gallery",
-  title: "rimmi ~ gallery",
+  title: "Gallery",
   headline: <>I take photos.</>,
   images: [
     {
