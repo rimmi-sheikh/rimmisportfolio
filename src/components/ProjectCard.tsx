fd615f6a0891e008129f3f6fed559cfdd7577ca9
Rimmal Sheikh <rimmalsheikh27@gmail.com>
2025-05-22 22:24:43 +0500

    yep

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -69,7 +69,7 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({
                   style={{ margin: "0", width: "fit-content" }}
                   href={href}
                 >
-                  <Text variant="body-default-s">Read the ramble</Text>
+                  <Text variant="body-default-s">Read more</Text>
                 </SmartLink>
               )}
               {link && (
