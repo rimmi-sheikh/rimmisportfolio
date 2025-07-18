90eee7aa6be856243ac831d0fba56d0276d33622
Rimmal Sheikh <rimmalsheikh27@gmail.com>
2025-07-18 22:28:07 +0500

    hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,7 +93,7 @@ export default function Home() {
               id="work"
               data-border="rounded"
               href="/work"
-              variant="primary" // Changed to primary for different color
+              variant="tertiary" // Changed to primary for different color
               size="m"         // Same size
               arrowIcon        // Same arrow
             >
