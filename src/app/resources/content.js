77b1d5c745a9b96f6c4ef6335b4f3af86f2d15ad
Rimmal Sheikh <rimmalsheikh27@gmail.com>
2025-07-19 00:06:10 +0500

    RAH

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -91,8 +91,8 @@ const about = {
       <>
       I am an engineering enthusiast with a passion for electrical systems, environmental technology, 
       with a surprising fondness for bridges (made of cardboard, spaghetti, or anything solid really). I merge hardware, AI, and sustainability to create 
-      impactful solutions—from air quality monitoring to flood detection systems. Whether I'm debugging code 
-      or building a car chassis from scratch, my work bridges the gap between practicality and ingenuity—
+      impactful solutions like air quality monitoring and flood detection systems. Whether I'm debugging code 
+      or building a car chassis from scratch, my work bridges the gap between practicality and ingenuity,
       sometimes literally.
     </>
     ),
